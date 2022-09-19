c9c8291c51cebc322c8a5fdae1d82e64cb895d21
VinceDeslo <vincent.desloover.dev@gmail.com>
2022-09-19 11:48:48 -0400

    Theme tweaking and const extraction.

--- src/tui/main.rs
+++ src/tui/main.rs
@@ -1,14 +1,23 @@
 use cursive::views::{Dialog, TextView};
+use cursive::align::HAlign;
 
+const THEME_PATH: &str = "themes/dark.toml";
+const TITLE: &str = "~ Ummon ~";
+const UMMON_QUOTE: &str = "\nWe inhabit the In-between\n Stitching small singularities\n Like lattice crystals";
+const BUTTON_OPTIONS: [&str; 2] = ["Cancel", "Fishstick!"];
 
 fn main() {
     let mut siv = cursive::default();
 
-    siv.load_theme_file("themes/dark.toml").unwrap();
+    siv.load_theme_file(THEME_PATH).unwrap();
 
-    let text_view = TextView::new("Hellow Cursive");
-    let dialog = Dialog::around(text_view)
-        .title("Cursive").button("Quit", |s| s.quit());
+    let text_view = TextView::new(UMMON_QUOTE)
+        .h_align(HAlign::Center);
+    
+        let dialog = Dialog::around(text_view)
+        .title(TITLE)
+        .button(BUTTON_OPTIONS[0], |s| s.quit())
+        .button(BUTTON_OPTIONS[1], |s| s.quit());
 
     siv.add_layer(dialog);
 
